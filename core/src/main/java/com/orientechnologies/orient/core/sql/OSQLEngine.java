9833fc0c4cbb361d80cc3b7835542dc2fa62739c
Tglman <tglman@tglman.com>
2024-11-20 22:37:03 +0000

    chore: add cause to parse exception

--- core/src/main/java/com/orientechnologies/orient/core/sql/OSQLEngine.java
+++ core/src/main/java/com/orientechnologies/orient/core/sql/OSQLEngine.java
@@ -23,6 +23,7 @@ import static com.orientechnologies.common.util.OClassLoaderHelper.lookupProvide
 
 import com.orientechnologies.common.collection.OMultiCollectionIterator;
 import com.orientechnologies.common.collection.OMultiValue;
+import com.orientechnologies.common.exception.OException;
 import com.orientechnologies.common.log.OLogManager;
 import com.orientechnologies.common.log.OLogger;
 import com.orientechnologies.common.util.OCallable;
@@ -98,7 +99,7 @@ public class OSQLEngine {
       List<OStatement> result = osql.parseScript();
       return result;
     } catch (ParseException e) {
-      throw new OCommandSQLParsingException(e, "");
+      throw OException.wrapException(new OCommandSQLParsingException(e, ""), e);
     }
   }
 
@@ -109,7 +110,7 @@ public class OSQLEngine {
       OOrBlock result = osql.OrBlock();
       return result;
     } catch (ParseException e) {
-      throw new OCommandSQLParsingException(e, "");
+      throw OException.wrapException(new OCommandSQLParsingException(e, ""), e);
     }
   }
 
@@ -120,7 +121,7 @@ public class OSQLEngine {
       OSecurityResourceSegment result = osql.SecurityResourceSegment();
       return result;
     } catch (ParseException e) {
-      throw new OCommandSQLParsingException(e, "");
+      throw OException.wrapException(new OCommandSQLParsingException(e, ""), e);
     }
   }
 
